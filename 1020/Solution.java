a5f26ba429e23ae6925b146a6780ea3d5c58b1d2
Guilherme Pessoa <gui.senai@live.com>
2023-01-26 07:25:52 -0300

    Resolução exercicio 1020 beecrowd

---
+++ 1020/Solution.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Solution{
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int idadeDias;
+        int anos; 
+        int mes;
+        int dias;
+        int resto;
+
+        idadeDias = teclado.nextInt();
+
+
+        anos = idadeDias / 365;
+        resto = idadeDias % 365;
+
+        mes = resto / 30;
+        dias = resto % 30;
+
+
+        System.out.println(anos +  "Ano(s)");
+        System.out.println(mes +  "Mes(es)");
+        System.out.println(dias +  " Dia(s)");
+
+
+
+
+
+
+
+        teclado.close();
+
+    }
+}
